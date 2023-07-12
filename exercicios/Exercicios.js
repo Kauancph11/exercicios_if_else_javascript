a38edc884dd97b1ec1916cbd397378805fdd9d45
Kauan <kauancph11@gmail.com>
2023-07-11 21:36:40 -0300

    exercicios

---
+++ exercicios/Exercicios.js
@@ -0,0 +1,74 @@
+let prompt = require("prompt-sync")()
+
+
+
+
+// let numero = parseInt(prompt("Digite um numero: "))
+// let resultado = parseInt(numero % 2)
+
+
+
+// if (resultado == 0) {
+//     console.log("Esse numero e par")
+    
+// }else{
+//     console.log("Esse numero e impar")
+// }
+
+// let nome = prompt("Digite seu nome: ")
+// let cargo = prompt("Digite seu cargo: ")
+// let salario = parseFloat (prompt("Digite seu salario: "))
+
+
+// if (salario < 1000) {
+//     acrescimo = parseFloat(salario * 0.1)
+//     console.log(nome)
+//     console.log(cargo)
+//     console.log("Seu salario e: " + (salario + acrescimo))
+// }else{
+//     console.log(nome)
+//     console.log(cargo)
+//     console.log("Seu salario e: " + salario)
+// }
+
+// let salario = parseFloat(prompt("Digite seu salario: "))
+
+// if (salario < 500) {
+//     ajuste = parseFloat(salario * 0.3)
+//     console.log("Seu salario e: " + (salario + ajuste))
+// }else{
+//     console.log("Seu salario e: " + salario )
+// }
+
+// let numero = parseInt(prompt("Digite um numero: "))
+
+// if (numero >= 0) {
+//     console.log("Esse numero e positivo")
+// }else{
+//     console.log("Esse numero e negativo")
+// }
+
+
+// let numero = parseInt(prompt("Digite o primeiro numero: "))
+// let numero2 = parseInt(prompt("Digite o segundo numero: "))
+
+// if (numero > numero2) {
+//     console.log("Esse numero e maior: " + numero)
+// }else{
+//     console.log("Esse numero e maior: " + numero2)
+// }
+
+// let nota1 = parseFloat(prompt("Digite a primeira nota: "))
+// let nota2 = parseFloat(prompt("Digite a segunda nota: "))
+// let nota3 = parseFloat(prompt("Digite a terceira nota: "))
+// let nota4 = parseFloat(prompt("Digite a quarta nota: "))
+
+// media = (nota1 + nota2 + nota3 + nota4) / 4
+
+// if (media >= 7.0) {
+//     console.log("Aprovado sua nota e: " + media)
+// }else if(media < 7.0 && media >= 5.0){
+//     console.log("Recuperacao sua nota e: " + media)
+// }else{
+//     console.log("Reprovado sua nota e: " + media)
+// }
